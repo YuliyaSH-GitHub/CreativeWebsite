8d9d744ae8fbcb04edac03eef3f5591a0b7e6459
Yuliya <shishova-yg@yandex.ru>
2021-06-30 17:16:38 +0300

    добавлен адаптив блоку b-howWeWorks

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,7 +2,6 @@ const swiper = new Swiper('.b-howWeWorks__slider', {
     edgeSwipeDetection: true,
     grabCursor: true,
     slideToClickedSlide: true,
-    //    slidesPerView: 2.5,
     slidesPerView: 3.4,
     spaceBetween: 10,
     // centeredSlides: true,
