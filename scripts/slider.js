e7341a62a75c6ed92f98a73a92674f8f243ad814
Yuliya <shishova-yg@yandex.ru>
2021-06-30 15:23:00 +0300

    добавлен слайдер и текстовый контент в блок b-howWeWorks

---
+++ scripts/slider.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.b-howWeWorks__slider', {
+    edgeSwipeDetection: true,
+    grabCursor: true,
+    slideToClickedSlide: true,
+    //    slidesPerView: 2.5,
+    slidesPerView: 3.4,
+    spaceBetween: 10,
+    // centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 4000,
+    },
+});
